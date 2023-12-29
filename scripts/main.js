be62109960c57f8f72821b1e9bf6704ac2190ae0
angelnalubowa <angelnalubowa97@gmail.com>
2023-12-30 00:14:13 +0300

    operator precedence

--- scripts/main.js
+++ scripts/main.js
@@ -26,4 +26,21 @@ document.getElementById("p3").innerHTML = "Student? " + student;
 //arithmetic expressions(operands, operators)
 let students = 20;
 student += 1; //students = students + 1;
-students -= 1; //students = students
+students -= 1; //students = students -1;
+students *= 2;//student = student * 2;
+students /= 2;//students = students /2;
+console.log(students);
+
+//modulous: remainder
+let extrastudents = students % 3;
+console.log(extrastudents);
+
+/*operator precedence
+1. parenthesis()
+2. exponents
+3. multiplication & division
+4. addition & subtraction
+*/
+
+let results = 1 + 2 * (3 + 4);
+console.log(results);
