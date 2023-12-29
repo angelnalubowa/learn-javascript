89de4baf76316339596654c6e88fba35dec5eeb7
angelnalubowa <angelnalubowa97@gmail.com>
2023-12-29 23:06:16 +0300

    lesson one

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
+//introduction
 console.log("Hello world");
+console.log("We are leraning javascript");
+window.alert("WE GOT THIS!");
+
+//variables: a container for storing data
+/*let age;...declaration
+age = 21;...assignment*/
+let firstname = "Angel";//strings
+let age = 21;//numbers
+let student = true//booleans
+let number = "1";//string
+
+age = age + 1;
+number = number + 5;
+
+console.log("Hello", firstname);
+console.log("Youre probably", age, "this year");
+console.log("Student?", student);
+console.log(number);
+
+document.getElementById("p1").innerHTML = "Hello " + firstname;
+document.getElementById("p2").innerHTML = "youre probably " + age + " this year";
+document.getElementById("p3").innerHTML = "Student? " + student;
