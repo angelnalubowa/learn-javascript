046ee5ae82b69f9ee4f0588609541c92c07c052f
angelnalubowa <angelnalubowa97@gmail.com>
2024-01-11 23:08:20 +0300

    finally

--- scripts/main.js
+++ scripts/main.js
@@ -138,13 +138,16 @@ let count=0;
 
 document.getElementById("increaseBtn").onclick = function(){
     count+=1;
-    document.getElementById("countLabel").innerHTML = count;
+    console.log(count);
+    document.getElementById("contLabel").innerHTML =  count ;
 }
 document.getElementById("resetBtn").onclick = function(){
     count=0;
-    document.getElementById("countLabel").innerHTML = count;
+    console.log(count);
+    document.getElementById("contLabel").innerHTML =  count ;
 }
 document.getElementById("decreaseBtn").onclick = function(){
     count-=1;
-    document.getElementById("countLabel").innerHTML = count;
+    console.log(count);
+    document.getElementById("contLabel").innerHTML =  count ;
 }
